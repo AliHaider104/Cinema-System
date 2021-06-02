a0f80df5f245ca5460b8024fbdbdbfe56dd7aaa5
AliHaider104 <salihaider699@gmail.com>
2021-06-02 16:23:44 +0500

    Final Commit

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            return View(Class1.getUsers());
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View(Class1.getUsers());
+        }
+
+        public ActionResult save(String EMAIL, String PASSWORD)
+        {
+            if (Class1.CHECK_USER(EMAIL, PASSWORD) == true)
+                return View("HOME_PAGE_WITH_FUNCTIONS");
+            else
+                return View("LOGIN_FAIL");
+        }
+
+        public ActionResult create_user(String Uname, String Uemail, String Umobile, String Upassword)
+        {
+            if (Class1.INSERT_USER(Uname, Uemail, Umobile, Upassword)==true)
+                return View("Inserted");
+            else
+                return View("notInserted");
+        }
+
+        public ActionResult HOME_PAGE_WITH_FUNCTIONS()
+        {
+            return View();
+        }
+
+        public ActionResult LOGIN_FAIL()
+        {
+            return View();
+        }
+
+        public ActionResult REQUIRED_LOGIN()
+        {
+            return View();
+        }
+
+        public ActionResult Inserted()
+        {
+            return View();
+        }
+
+        public ActionResult notInserted()
+        {
+            return View();
+        }
+
+        public ActionResult NOWSHOWING()
+        {
+            return View(Class1.getMovies());
+        }
+
+        public ActionResult Book1()
+        {
+            if (Class1.BOOK_TICKET(1, "12-3-2019"))
+                return View("Booked");
+            else
+                return View("notBooked");
+        }
+
+        public ActionResult Book2()
+        {
+            if (Class1.BOOK_TICKET(2, "12-3-2019"))
+                return View("Booked");
+            else
+                return View("notBooked");
+
+        }
+
+        public ActionResult Book3()
+        {
+            if (Class1.BOOK_TICKET(3, "12-1-2019"))
+                return View("Booked");
+            else
+                return View("notBooked");
+
+        }
+
+        public ActionResult Book4()
+        {
+            if (Class1.BOOK_TICKET(4, "12-2-2019"))
+                return View("Booked");
+            else
+                return View("notBooked");
+
+        }
+
+        public ActionResult Book5()
+        {
+            if (Class1.BOOK_TICKET(5, "12-3-2019"))
+                return View("Booked");
+            else
+                return View("notBooked");
+
+        }
+
+        public ActionResult NOT_AVAILABLE()
+        {
+            return View();
+        }
+
+        public ActionResult Booked()
+        {
+            return View();
+        }
+
+        public ActionResult notBooked()
+        {
+            return View();
+        }
+
+        public ActionResult EVENT_BOOKING()
+        {
+            return View();
+        }
+
+        public ActionResult BOOK_EVENT()
+        {
+            if (Class1.BOOK_EVENT(1))
+                return View("Booked");
+            else
+                return View("notBooked");
+        }
+
+        public ActionResult BOOK_EVENT1()
+        {
+            if (Class1.BOOK_EVENT(2))
+                return View("Booked");
+            else
+                return View("notBooked");
+        }
+
+        public ActionResult BOOK_EVENT2()
+        {
+            if (Class1.BOOK_EVENT(3))
+                return View("Booked");
+            else
+                return View("notBooked");
+        }
+
+    }
+}
---
+++ WebApplication3/Models/Class1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebApplication3.Models
+{
+    public class Class1
+    {
+        public static SqlDataReader getUsers()
+        {
+            string connectionString = @"Data Source=DESKTOP-BK162V0; Initial Catalog=CINEMA_SYSTEM; User=ali; Password=123;";
+            //string connectionString = @"Data Source=(localdb)\Name; Initial Catalog=connection;Integrated Security=True;";
+            //string connectionString ="data source=DESKTOP-12I97AL\\SQLEXPRESS; Initial Catalog=HRschema;Integrated Security=true";
+            string queryString = "select * from [user]";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(queryString, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                return reader;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public static SqlDataReader getMovies()
+        {
+            string connectionString = @"Data Source=DESKTOP-BK162V0; Initial Catalog=CINEMA_SYSTEM; User=ali; Password=123;";
+
+            string queryString = "select * from m";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(queryString, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                return reader;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public static bool INSERT_USER(String Uname, String Uemail, String Umobile, String Upassword)
+        {
+            string connectionString = @"Data Source=DESKTOP-BK162V0; Initial Catalog=CINEMA_SYSTEM; User=ali; Password=123;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlCommand cmd;
+            int result = 0;
+
+
+            try
+            {
+                connection.Open();
+                cmd = new SqlCommand("InsertUser", connection);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Add("@name", SqlDbType.NVarChar, 50).Value = Uname;
+                cmd.Parameters.Add("@email", SqlDbType.NVarChar, 50).Value = Uemail;
+                cmd.Parameters.Add("@mobileNo", SqlDbType.NVarChar, 50).Value = Umobile;
+                cmd.Parameters.Add("@password", SqlDbType.NVarChar, 50).Value = Upassword;
+                cmd.Parameters.Add("@status", SqlDbType.Int).Direction = ParameterDirection.Output;
+                cmd.ExecuteNonQuery();
+                result = Convert.ToInt32(cmd.Parameters["@status"].Value);
+
+                if (result == 1)
+                    return true;
+                else
+                    return false;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public static bool CHECK_USER(String name, String password)
+        {
+
+            string connectionString = @"Data Source=DESKTOP-BK162V0; Initial Catalog=CINEMA_SYSTEM; User=ali; Password=123;";
+            
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlCommand cmd;
+            int result = 0;
+
+
+            try
+            {
+                connection.Open();
+                cmd = new SqlCommand("[Login]", connection);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Add("@Uemail", SqlDbType.NVarChar, 50).Value = name;
+                cmd.Parameters.Add("@Upassword", SqlDbType.NVarChar, 50).Value = password;
+                cmd.Parameters.Add("@status", SqlDbType.Int).Direction = ParameterDirection.Output;
+                cmd.ExecuteNonQuery();
+                result = Convert.ToInt32(cmd.Parameters["@status"].Value);
+
+                if (result == 1)
+                    return true;
+                else
+                    return false;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public static bool BOOK_TICKET(int MovieId, String Date)
+        {
+
+            string connectionString = @"Data Source=DESKTOP-BK162V0; Initial Catalog=CINEMA_SYSTEM; User=ali; Password=123;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlCommand cmd;
+            int result = 0;
+
+
+            try
+            {
+                connection.Open();
+                cmd = new SqlCommand("addinBooking", connection);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Add("@movieid", SqlDbType.Int).Value = MovieId;
+                cmd.Parameters.Add("@date", SqlDbType.Date).Value = Date;
+                cmd.Parameters.Add("@status", SqlDbType.Int).Direction = ParameterDirection.Output;
+                cmd.ExecuteNonQuery();
+                result = Convert.ToInt32(cmd.Parameters["@status"].Value);
+
+                if (result == 1)
+                    return true;
+                else
+                    return false;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+        }
+
+        public static bool BOOK_EVENT(int Eventid)
+        {
+
+            string connectionString = @"Data Source=DESKTOP-BK162V0; Initial Catalog=CINEMA_SYSTEM; User=ali; Password=123;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlCommand cmd;
+            int result = 0;
+
+
+            try
+            {
+                connection.Open();
+                cmd = new SqlCommand("EVENTBOOKINGG", connection);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.Add("@id", SqlDbType.Int).Value = Eventid;
+                cmd.Parameters.Add("@status", SqlDbType.Int).Direction = ParameterDirection.Output;
+                cmd.ExecuteNonQuery();
+                result = Convert.ToInt32(cmd.Parameters["@status"].Value);
+
+                if (result == 1)
+                    return true;
+                else
+                    return false;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+        }
+    }
+}
